e07e83871011b16df379cf71b286928af9283e8d
HalforcNull <yaorunan@gmail.com>
2018-11-06 12:44:59 -0600

    add definition of kernel function

--- HW8-2.R
+++ HW8-2.R
@@ -15,3 +15,16 @@ tN <- sin(2*pi*xN) + rnorm(n.sim,0,sqrt(1/beta))
 quartz()
 plot(x,sin(2*pi*x),type="l",col="green")
 points(xN,tN,col="blue")
+
+
+# Changes from HW7
+#   1. aN. In this case, we have a 2*n length vector, to contain c(aN, aN_hat)
+#   2. Dmat. D_mat is changed since the quadratic part of the problem is changed
+#   3. dvect. dvect changed as we discussed during class
+#   4. Amat and bvect. Since the constrain of the problem is changed
+#   5. Kernel function. ker_1 = abs(x1-x2)
+#                       ker_2 = min( abs( (x1-0.25)^2 - (x2-0.25)^2 ), abs( (x1-0.75)^2 - (x2-0.75)^2) ))
+
+
+
+
