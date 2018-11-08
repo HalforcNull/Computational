2b5ac644caebed8fe94fb136ade70222c6575562
HalforcNull <yaorunan@gmail.com>
2018-11-08 16:04:37 -0600

    sub functions

--- HW8-3.R
+++ HW8-3.R
@@ -25,20 +25,42 @@ points(xN,tN,col="blue")
 
 
 
+# kenrel 3
+ker_3 <- function(x,y){
+    return(exp(-0.5 * 16 * 1.414 * (x-y) * (x-y)))
+}
 
 
+# Gram matrix calculate:
+#      since the x is one dim. don't need call apply anymore
+# Calculate grad matrix
+## sub function: calculate the diag
+Gram_diag_element <- function(x, distanceFun){
+    return(distanceFun(x,x))
+}
 
+GramMat <- function(xVec,kernelFun){
+    diag.vec <- Gram_diag_element(xVec, kernelFun)
+    if(length(diag.vec) != length(xVec) ){
+        Gram_diag <- diag( rep(diag.vec[1], length(xVec)) ) # n * n diag
+    }else{
+        Gram_diag <- diag( diag.vec ) # n * n diag
+    }
+    
+    Gram_dist <- proxy::dist(xVec, method=kernelFun) # n-1 * n-1 mat
+    
+    return(as.matrix(Gram_dist) + Gram_diag)
+}
 
+# sigma function
+calcSigma <- function(alpha, beta, phi){
+    return(solve(diag(alpha) + beta * t(phi) %*% phi))
+}
 
-
-
-
-
-
-
-
-
-
+# mu function
+calcMu <- function(beta, sigma, phi, tN){
+    return(beta * sigma %*% t(Phi) %*% tN)
+}
 
 
 # gamma: used to udpate alpha and beta
