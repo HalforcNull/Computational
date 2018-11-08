62748d84472b3c87ecbb2aa193e24fb0d0f11a5b
HalforcNull <yaorunan@gmail.com>
2018-11-08 15:56:13 -0600

    function for update alpha and beta

--- HW8-3.R
+++ HW8-3.R
@@ -15,3 +15,47 @@ tN <- sin(2*pi*xN) + rnorm(n.sim,0,sqrt(1/beta))
 quartz()
 plot(x,sin(2*pi*x),type="l",col="green")
 points(xN,tN,col="blue")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# gamma: used to udpate alpha and beta
+calcGammaVec <- function(sigma, currentAlpha){
+    diagSigma <- rowSums( sigma * diag(dim(sigma)[1]) )
+    gamma <- 1 - currentAlpha * diagSigma
+    return(gamma)
+}
+
+# update alpha 
+updateAlpha <- function(mu, gamma){
+    return(gamma / mu^2)
+}
+
+# update beta
+updateBetaInv <- function(t, phi, mu, N, gamma ){
+    dis.sqr <- dist(rbind(t, phi %*% mu))^2
+    return(dis.sqr / (N - sum(gamma)))
+}
+
