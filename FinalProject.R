774116c0612644c6de14f1e2737911cdfc272c37
HalforcNull <yaorunan@gmail.com>
2018-12-05 14:21:33 -0600

    Kernel and gram matrix

--- FinalProject.R
+++ FinalProject.R
@@ -5,3 +5,20 @@ library(mvtnorm)
 library(quadprog)
 library(Matrix)
 
+
+load('ink.training.rdata')
+
+## Step 1. Data re-format
+dat.training.S1 <- apply(ink.training.rdata[,,,1], 3, as.numeric)
+dat.training.S2 <- apply(ink.training.rdata[,,,2], 3, as.numeric)
+
+dat.training.X <- cbind(dat.training.S1, dat.training.S2)
+dat.training.Tar <- c(rep(1, 22), rep(2,22))
+
+
+
+
+
+
+
+
