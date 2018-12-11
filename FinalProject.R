328a629cfeefc8e47f924098b15739fe36dbd81e
HalforcNull <yaorunan@gmail.com>
2018-12-11 11:01:54 -0600

    data loading is wrong

--- FinalProject.R
+++ FinalProject.R
@@ -9,8 +9,8 @@ library(Matrix)
 load('ink.training.rdata')
 
 ## Step 1. Data re-format
-dat.training.S1 <- apply(ink.training.rdata[,,,1], 3, as.numeric)
-dat.training.S2 <- apply(ink.training.rdata[,,,2], 3, as.numeric)
+dat.training.S1 <- apply(ink.training.dat[,,,1], 3, as.numeric)
+dat.training.S2 <- apply(ink.training.dat[,,,2], 3, as.numeric)
 
 dat.training.X <- cbind(dat.training.S1, dat.training.S2)
 dat.training.Tar <- c(rep(1, 22), rep(2,22))
